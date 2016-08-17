c6b0890b3cd099530f81618abff3a96d06b404cd
fyccctv <fyc0629@sina.com>
2016-08-17 15:22:03 +0800

    添加项目文件。

---
+++ App4/Notify.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    public class Notify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string title { get; set; }
+        private int year { get; set; }
+        private string desc { get; set; }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                NotifyPropertyChanged(nameof(Title));
+            }
+        }
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                year = value;
+                NotifyPropertyChanged("Year");
+            }
+        }
+        public string Desc
+        {
+            get { return desc; }
+            set
+            {
+                desc = value;
+                NotifyPropertyChanged("Desc");
+            }
+        }
+        protected void NotifyPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+    }
+}
